b499df19f5e0ba4c5045c79b149742a40cf5b3aa
kkkk <junkkkklv@sina.com>
2019-08-29 17:24:45 +0800

    Firstly upload project files to github. --20190828

---
+++ model/ItemCF.py
@@ -0,0 +1,335 @@
+# -*- coding: utf-8 -*-
+# @Author: Klaus
+# @Date:   2019-08-04 23:04:28
+# @Last Modified by:   kkkk
+# @Last Modified time: 2019-08-29 14:53:39
+# ------------------------------------------------------------------------------
+# 代码模块化在不同的py文件中，用itemCF来做比赛的Baseline
+# 将比赛数据转化为 {user1:{item1, item2, item3, ...},
+#                   user2:{item1, item2, item3, ...},
+#                   ....}
+# 后续可以考虑以 数组的形式来保存数据额和完成代码
+# --->
+# TO DO LIST:
+# LFM隐语义模型
+# ------------------------------------------------------------------------------
+import sys
+sys.path.append("..")
+
+import math
+from utility.metrics import Metric
+from utility.dataset import Dataset
+from utility.decora import timmer
+
+
+def ItemCF(train, K, N):
+    '''
+    :params: train, 训练数据集
+    :params: K, 超参数，设置取TopK相似物品数目
+    :params: N, 超参数，设置取TopN推荐物品数目
+    :return: GetRecommendation, 推荐接口函数
+    '''
+    # 计算 items--items 的稀疏矩阵
+    # 两两item同时出现的次数 #(item1, item2). {item1:{item1,item2,item3,..}, item2:{item1,item2,item3,..},...}
+    cmat = {}
+    num = {}         # 单一item出现的次数 #item1  (相当于行索引的item出现的次数)
+    for user in train.keys():
+        items = train[user]
+        for item1 in items:
+            if item1 not in num.keys():  # 单一item出现的次数
+                num[item1] = 0
+            num[item1] += 1
+            if item1 not in cmat.keys():
+                cmat[item1] = {}
+            for item2 in items:
+                if item2 == item1:
+                    continue
+                if item2 not in cmat[item1]:
+                    cmat[item1][item2] = 0
+                cmat[item1][item2] += 1
+
+    # 计算余弦相似度
+    sim = {}          # 初始化 相似度矩阵
+    for i in cmat.keys():
+        sim[i] = {}   # 初始化 sim[i]，确保sim[i]也是dict
+        for j, cij in cmat[i].items():
+            sim[i][j] = cij / math.sqrt(num[i] * num[j])
+
+    # 按照相似度的值对矩阵的每一行进行降序排序
+    sim_item_sorted = {}
+    for key, values in sim.items():
+        sim_item_sorted[key] = sorted(
+            values.items(), key=lambda x: x[1], reverse=True)[:K]
+        # sorted函数返回的是列表 list
+
+    # 为待推荐的用户获取推荐接口函数
+    def GetRecommendation(user):
+        rank = {}                                   # 待推荐列表  {item1:rank1, item2:rank2,...}
+        # 用户见过的item列表 [item1, item2, item3, ...]
+        interacted_items = set(train[user])
+        # print(interacted_items, '---------------')
+        # 根据相似度高的用户的列表对user进行推荐（去掉user见过的item）
+        for item in train[user]:                    # 遍历user的物品列表
+            for i, _ in sim_item_sorted[item]:            # 与排序后的topK个相似物品进行相似度计算
+                if i not in interacted_items:       # topK中的item不在user的已有列表中
+                    if i not in rank.keys():        # topK中的item不在待推荐列表中
+                        rank[i] = 0              # 不在rank中则添加进去并赋初值为0
+                    # topK与用户已有items的两两共现余弦相似度矩阵
+                    rank[i] += sim[item][i]
+        # 对rank字典排序，获得 topN 对应的 item
+        rank_sorted = sorted(rank.items(), key=lambda x: x[1], reverse=True)[:N]
+        '''若只保存 item，不需要#item: [item1, item2, item3, ...]'''
+#         rank_sorted = list(map(lambda x: x[0], rank_sorted))
+        # 返回值是列表
+        return rank_sorted
+
+    return GetRecommendation
+
+
+def ItemIUF(train, K, N):
+    '''
+    :params: train, 训练数据集
+    :params: K, 超参数，设置取TopK相似物品数目
+    :params: N, 超参数，设置取TopN推荐物品数目
+    :return: GetRecommendation, 推荐接口函数
+    '''
+    # 计算 items--items 的稀疏矩阵
+    # 两两item同时出现的次数 #(item1, item2). {item1:{item1,item2,item3,..}, item2:{item1,item2,item3,..},...}
+    cmat = {}
+    num = {}         # 单一item出现的次数 #item1  (相当于行索引的item出现的次数)
+    for user in train.keys():
+        items = train[user]
+        for item1 in items:
+            if item1 not in num.keys():  # 单一item出现的次数
+                num[item1] = 0
+            num[item1] += 1
+            if item1 not in cmat.keys():
+                cmat[item1] = {}
+            for item2 in items:
+                if item2 == item1:
+                    continue
+                if item2 not in cmat[item1]:
+                    cmat[item1][item2] = 0
+                cmat[item1][item2] += 1 / math.log(1 + len(items))
+
+    # 计算余弦相似度
+    sim = {}          # 初始化 相似度矩阵
+    for i in cmat.keys():
+        sim[i] = {}   # 初始化 sim[i]，确保sim[i]也是dict
+        for j, cij in cmat[i].items():
+            sim[i][j] = cij / math.sqrt(num[i] * num[j])
+
+    # 按照相似度的值对矩阵的每一行进行降序排序
+    sim_item_sorted = {}
+    for key, values in sim.items():
+        sim_item_sorted[key] = sorted(values.items(), key=lambda x: x[1], reverse=True)[:K]
+        # sorted函数返回的是列表 list
+
+    # 为待推荐的用户获取推荐接口函数
+    def GetRecommendation(user):
+        rank = {}                                   # 待推荐列表  {item1:rank1, item2:rank2,...}
+        # 用户见过的item列表 [item1, item2, item3, ...]
+        interacted_items = set(train[user])
+        # 根据相似度高的用户的列表对user进行推荐（去掉user见过的item）
+        for item in train[user]:                    # 遍历user的物品列表
+            for i, _ in sim_item_sorted[item]:            # 与排序后的topK个相似物品进行相似度计算
+                if i not in interacted_items:       # topK中的item不在user的已有列表中
+                    if i not in rank.keys():        # topK中的item不在待推荐列表中
+                        rank[i] = 0              # 不在rank中则添加进去并赋初值为0
+                    # topK与用户已有items的两两共现余弦相似度矩阵
+                    rank[i] += sim[item][i]
+        # 对rank字典排序，获得 topN 对应的 item
+        rank_sorted = sorted(
+            rank.items(), key=lambda x: x[1], reverse=True)[:N]
+        '''若只保存 item，不需要#item: [item1, item2, item3, ...]'''
+#         rank_sorted = list(map(lambda x: x[0], rank_sorted))
+        # 返回值是列表
+        return rank_sorted
+
+    return GetRecommendation
+
+
+def ItemCF_Norm(train, K, N):
+    '''
+    :params: train, 训练数据集
+    :params: K, 超参数，设置取TopK相似物品数目
+    :params: N, 超参数，设置取TopN推荐物品数目
+    :return: GetRecommendation, 推荐接口函数
+    '''
+    # 计算 items--items 的稀疏矩阵
+    # 两两item同时出现的次数 #(item1, item2). {item1:{item1,item2,item3,..}, item2:{item1,item2,item3,..},...}
+    cmat = {}
+    num = {}         # 单一item出现的次数 #item1  (相当于行索引的item出现的次数)
+    for user in train.keys():
+        items = train[user]
+        for item1 in items:
+            if item1 not in num.keys():  # 单一item出现的次数
+                num[item1] = 0
+            num[item1] += 1
+            if item1 not in cmat.keys():
+                cmat[item1] = {}
+            for item2 in items:
+                if item2 == item1:
+                    continue
+                if item2 not in cmat[item1]:
+                    cmat[item1][item2] = 0
+                cmat[item1][item2] += 1 / math.log(1 + len(items))
+
+    # 计算余弦相似度
+    sim = {}          # 初始化 相似度矩阵
+    for i in cmat.keys():
+        sim[i] = {}   # 初始化 sim[i]，确保sim[i]也是dict
+        for j, cij in cmat[i].items():
+            sim[i][j] = cij / math.sqrt(num[i] * num[j])
+
+    '''相似度矩阵归一化'''
+    for i in sim.keys():
+        s_max = 0                # 每遍历一行之前先初始化s_max为0
+        for j in sim[i].keys():
+            if sim[i][j] >= s_max:
+                s_max = sim[i][j]
+            sim[i][j] /= s_max
+
+    # 按照相似度的值对矩阵的每一行进行降序排序
+    sim_item_sorted = {}
+    for key, values in sim.items():
+        sim_item_sorted[key] = sorted(
+            values.items(), key=lambda x: x[1], reverse=True)[:K]
+        # sorted函数返回的是列表 list
+
+    # 为待推荐的用户获取推荐接口函数
+    def GetRecommendation(user):
+        rank = {}                                   # 待推荐列表  {item1:rank1, item2:rank2,...}
+        # 用户见过的item列表 [item1, item2, item3, ...]
+        interacted_items = set(train[user])
+        # 根据相似度高的用户的列表对user进行推荐（去掉user见过的item）
+        for item in train[user]:                    # 遍历user的物品列表
+            # 与排序后的topK个相似物品进行相似度计算
+            for i, _ in sim_item_sorted[item]:
+                if i not in interacted_items:       # topK中的item不在user的已有列表中
+                    if i not in rank.keys():        # topK中的item不在待推荐列表中
+                        rank[i] = 0              # 不在rank中则添加进去并赋初值为0
+                    # topK与用户已有items的两两共现余弦相似度矩阵
+                    rank[i] += sim[item][i]
+        # 对rank字典排序，获得 topN 对应的 item
+        rank_sorted = sorted(
+            rank.items(), key=lambda x: x[1], reverse=True)[:N]
+        '''若只保存 item，不需要#item: [item1, item2, item3, ...]'''
+#         rank_sorted = list(map(lambda x: x[0], rank_sorted))
+        # 返回值是列表
+        return rank_sorted
+
+    return GetRecommendation
+
+
+class Experiment():
+    def __init__(self, M, K, N, fp=r'..\data\ml-1m\ratings.dat', method='ItemCF'):
+        '''
+        :params: M, 交叉验证实验次数
+        :params: K, TopK相似物品的个数
+        :params: N, TopN推荐物品的个数
+        :params: fp, 数据文件路径
+        :params: method, 推荐算法
+        '''
+        self.M = M
+        self.K = K
+        self.N = N
+        self.fp = fp
+        self.method = method
+        self.alg = {"ItemCF": ItemCF, "ItemIUF": ItemIUF,
+                    "ItemCF_Norm": ItemCF_Norm}
+
+    @timmer
+    def worker(self, train, test):
+        '''
+        :params: train, 训练数据集
+        :params: test, 测试数据集
+        :return: 各指标的值
+        '''
+        getRecommendation = self.alg[self.method](train, self.K, self.N)
+        metric = Metric(train, test, getRecommendation)
+        return metric.eval()
+
+    @timmer
+    def run(self):
+        metrics = {'Precision': 0, 'Recall': 0,
+                   'Coverage': 0, 'Popularity': 0}
+        dataset = Dataset(self.fp)
+        for ii in range(self.M):
+            train, test, _ = dataset.splitData(self.M, ii)
+            print('Experiment {}:'.format(ii))
+            metric = self.worker(train, test)
+            metrics = {k: metrics[k] + metric[k] for k in metrics}
+        metrics = {k: metrics[k] / self.M for k in metrics}
+        print('Average Result (M={}, N={}, ratio={}): {}'.format(
+            self.M, self.N, self.ratio, metrics))
+
+
+if __name__ == '__main__':
+    print('---------------------- ItemCF ----------------------')
+    # 1. ItemCF
+    M = 5
+    N = 10
+    for K in [5, 10, 20, 40, 80, 160]:
+        cf_exp = Experiment(M, K, N, method='ItemCF')
+        cf_exp.run()
+    print('---------------------- ItemIUF ----------------------')
+    M = 5
+    K = 10
+    iuf_exp = Experiment(M, K, N, method='ItemIUF')
+    iuf_exp.run()
+    print('\n')
+    print('---------------------- ItemCF_Norm ----------------------')
+    cfnorm_exp = Experiment(M, K, N, method='ItemCF_Norm')
+    cfnorm_exp.run()
+
+# ---------------------- ItemCF ----------------------
+# Func loadData, run time: 1.3587447
+# Func splitData, run time: 1.5897664999999999
+# count: 6035
+# Metric: {'Precision': 29.69, 'Recall': 8.95, 'Coverage': 22.45, 'Popularity': 7.101221}
+# Func worker, run time: 96.853724
+# Done!!
+
+# Func run, run time: 99.9004774
+# Func loadData, run time: 1.3753652999999986
+# Func splitData, run time: 1.8933593999999943
+# count: 6035
+# Metric: {'Precision': 30.62, 'Recall': 9.22, 'Coverage': 19.55, 'Popularity': 7.182597}
+# Func worker, run time: 93.3659322
+# Done!!
+
+# Func run, run time: 96.75672019999999
+# Func loadData, run time: 1.354055800000026
+# Func splitData, run time: 1.6839875000000006
+# count: 6035
+# Metric: {'Precision': 30.22, 'Recall': 9.11, 'Coverage': 17.24, 'Popularity': 7.277239}
+# Func worker, run time: 95.8356875
+# Done!!
+
+# Func run, run time: 98.9685244
+# Func loadData, run time: 1.2680831999999782
+# Func splitData, run time: 1.6084959000000367
+# count: 6035
+# Metric: {'Precision': 29.46, 'Recall': 8.88, 'Coverage': 15.37, 'Popularity': 7.331447}
+# Func worker, run time: 96.5398199
+# Done!!
+
+# Func run, run time: 99.51386910000002
+# Func loadData, run time: 1.2554929000000357
+# Func splitData, run time: 1.542421499999989
+# count: 6035
+# Metric: {'Precision': 28.42, 'Recall': 8.56, 'Coverage': 13.49, 'Popularity': 7.349231}
+# Func worker, run time: 108.44748620000001
+# Done!!
+
+# Func run, run time: 111.3402949
+# Func loadData, run time: 1.2332144000000085
+# Func splitData, run time: 1.5512279000000149
+# count: 6035
+# Metric: {'Precision': 27.18, 'Recall': 8.19, 'Coverage': 12.06, 'Popularity': 7.32431}
+# Func worker, run time: 142.33041429999997
+# Done!!
+
+# Func run, run time: 145.2348804
+# [Finished in 652.1s]
---
+++ model/lfm_rec.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# @Author: Klaus
+# @Date:   2019-08-22 20:20:02
+# @Last Modified by:   kkkk
+# @Last Modified time: 2019-08-29 14:18:40
+# ------------------------------------------------------------------------------
+# 隐语义模型
+# ------------------------------------------------------------------------------
+import sys
+sys.path.append("..")
+
+from utility.decora import timmer
+from utility.metrics import Metric
+from utility.dataset import Dataset
+import numpy as np
+
+
+def LFM(train, ratio, F, N, alpha, lamb, step):
+    '''
+    :params: train, 训练数据
+    :params: ratio, 正负采样的比例 一般取1，保持数量相近
+    :params: F, 隐语义个数
+    :params: alpha, 初始学习率
+    :params: step, 迭代次数
+    :params: lamb, 正则化系数
+    :params: N, 推荐TopN物品的个数
+    :return: GetRecommendation, 获取推荐结果的接口
+    '''
+    # 提取总的item集合，即所有用户的不重复item
+    all_items = {}
+    for user in train.keys():
+        for item in train[user]:
+            if item not in all_items:
+                all_items[item] = 0
+            all_items[item] += 1
+    # [('a', {'aa': 4}), ('b', {'bb': 14}), ('c', {'cc': 24})]
+    all_items = list(all_items.items())
+    # 所有物品的列表list
+    items = [x[0] for x in all_items]
+    # 列表物品对应的流行度
+    pops = [x[1] for x in all_items]
+    # 即样本集只有正样本，正样本的流行度即为样品被不同用户喜欢的次数
+    print('items and their popularity: done')
+
+    # 负采样 ---> 保证正负样本数量比率均衡
+    @timmer
+    def RandomSelectNegSample(data, ratio):
+        new_data = {}       # 包含正负样本的数据集, 正样本兴趣rui为1，负样本兴趣rui为0
+        # 正样本  r_u1_i1 = data[user_1][item_1]
+        for user in data:
+            if user not in new_data.keys():
+                new_data[user] = {}
+            for item in data[user]:
+                new_data[user][item] = 1    # 用户user消费过的物品item，标记为1（表示正样本）
+        # 负样本
+        print("Negtiva sampling start.")
+        for user in new_data.keys():
+            seen = set(new_data[user])
+            num_pos = len(seen)
+            # 从所有物品items中根据流行度pops的值作为概率依据选择pum_pos*ratio*3个样本，依次将这些样本
+            # 与有过行为的样品seen做对比，只选取不在seen中的物品，取len(pos)*ratio个样本作为负样本
+            item = np.random.choice(items, int(
+                num_pos * ratio * 3), pops)      # 用法见tips
+            item_neg = []
+            count = 0
+            for element in item:
+                if element not in seen:
+                    item_neg.append(element)
+                    count += 1
+                if count > int(num_pos * ratio):   # 从负样本集合中取正负样本成比率的样本作为负样本
+                    break
+            new_data[user].update({x: 0 for x in item_neg})  # 负样本标记为0
+        print("Negtiva sampling end.")
+        return new_data
+
+    # 训练 随机梯度下降 --> 先初始化
+    P, Q = {}, {}       # Puk, Qki
+    # P：用户u的兴趣和第f个隐类的关系，Q:第f个隐类和物品i的关系
+    for user in train.keys():
+        P[user] = np.random.random(F)   # P: (u,f)   P[u]:(f,)
+    for item in items:
+        Q[item] = np.random.random(F)   # Q: (i,f)   Q[i]:(f,)   P[u]*Q[i]:(f,)
+    # 迭代
+    print('GSD start.')
+    for s in range(step):
+        data = RandomSelectNegSample(train, ratio)
+        for user in data.keys():
+            for item in data[user]:
+                eui = data[user][item] - \
+                    (P[user] * Q[item]).sum()  # 常量如0.76 float
+                P[user] += alpha * (eui * Q[item] - lamb * P[user])
+                Q[item] += alpha * (eui * P[user] - lamb * Q[item])
+                # (f, ) += const * (const * (f, ) - const * (f, ))
+        # SGD中需要设置每一次迭代之后调整学习率(衰减)
+        alpha *= 0.9
+    print('GSD end.')
+
+    # 获取接口函数
+    @timmer
+    def Recommendation(user):
+        seen_items = set(train[user])
+        recs = {}
+        for item in items:                  # 遍历所有物品
+            if item not in seen_items:      # 未消费过，则计算用户u对物品i的兴趣
+                # 兴趣rui公式： rui = (Puf * Qif).sum()
+                recs[item] = (P[user] * Q[item]).sum()
+        # 根据兴趣值recs[item]对recs中的item进行排序 降序，取topN
+        print('Get the top N items recommended for {}!'.format(user))
+        recs_top = sorted(recs.items(), key=lambda x: x[1], reverse=True)[:N]
+        return recs_top
+
+    return Recommendation
+
+
+# 实验测试
+class Experiment():
+    def __init__(self, M, N, ratio=1, F=100, alpha=0.02, step=1, lamb=0.01, fp='../data/ml-1m/ratings.dat'):
+        '''
+        params: M, 进行多少次实验
+        params: N, TopN推荐物品的个数
+        params: ratio, 正负样本比例
+        params: F, 隐语义个数
+        params: alpha, 学习率
+        params: step, 训练步数
+        params: lamb, 正则化系数
+        params: fp, 数据文件路径
+        '''
+        self.M = M
+        self.F = F
+        self.N = N
+        self.ratio = ratio
+        self.alpha = alpha
+        self.step = step
+        self.lamb = lamb
+        self.fp = fp
+        self.alg = LFM
+
+    @timmer
+    def worker(self, train, test):
+        '''
+        :params: train, 训练数据集
+        :params: test, 测试数据集
+        :return: 各指标的值
+        '''
+        recommendation = self.alg(
+            train, self.ratio, self.F, self.N, self.alpha, self.lamb, self.step)
+        metric = Metric(train, test, recommendation)
+        return metric.eval()
+
+    @timmer
+    def run(self):
+        metrics = {'Precision': 0, 'Recall': 0, 'Coverage': 0, 'Popularity': 0}
+        dataset = Dataset(self.fp)
+        for i in range(self.M):
+            train, test, _ = dataset.splitData(self.M, i)
+            print("Experiment {}:".format(i))
+            metric = self.worker(train, test)
+            metrics = {k: metrics[k] + metric[k] for k in metrics.keys()}
+        metrics = {k: metrics[k] / self.M for k in metrics.keys()}
+        print('Average Result (M={}, N={}, ratio={}): {}'.format(
+            self.M, self.N, self.ratio, metrics))
+
+
+if __name__ == '__main__':
+    M, N, ratio = 5, 10, 1
+    exp = Experiment(M, N, ratio=ratio)
+    exp.run()
